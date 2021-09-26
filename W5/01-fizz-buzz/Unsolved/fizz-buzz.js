08ab01579003ec2b50100ba5ae6a6a3810788b28
dboren <danielboren@att.net>
2021-09-26 17:46:02 -0500

    corrected comparison operator in iterator to use < instead of <=. Testing successfully throughout array now

--- W5/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ W5/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -6,7 +6,7 @@
 
 var fizzBuzz = function(arr) {
 
-    for (var i = 0; i <= arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
 
         if ((arr[i] % 3 === 0) && (arr[i] % 5 === 0)) {
 
