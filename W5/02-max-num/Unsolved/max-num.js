3dcd967d328a188af718d2ce7787d9cde794628c
dboren <danielboren@att.net>
2021-10-03 20:24:57 -0500

    tried moving currMax initial declaration outside of the loop but now loop is not running

--- W5/02-max-num/Unsolved/max-num.js
+++ W5/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,18 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+
+    var currMax = arr[0];
+
+    for (var i = 0; i <= arr.length; i++) {
+
+        if (currMax < arr[i]) {
+            currMax = arr[i];
+            console.log("currMax: ", currMax)
+            console.log("arr[i]: ", arr[i])
+
+        } else {
+            return arr[i];
+        }
+    }
+};
