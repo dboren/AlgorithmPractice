5d5ae11db264841aa5d14f444971bdb29a0b3557
dboren <danielboren@att.net>
2021-10-09 17:45:21 -0500

    wrote initial for loop to have value of vowel count increase by one if the character at each index value of the string is a vowel. Receiving error that a in conditional is not defined

--- W5/03-vowel-count/Unsolved/vowel-count.js
+++ W5/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,18 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+
+    let totalVowels = 0;
+
+    str.toLowerCase();
+
+    for (i = 0; i < str.length; i++) {
+
+        if (str[i] == a || e || i || o || u) {
+
+            totalVowels++;
+        }
+    }
+
+    return totalVowels;
+};
