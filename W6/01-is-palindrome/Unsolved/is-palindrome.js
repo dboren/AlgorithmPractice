d2136f8b6cde7dcc1cf9563c2505a39d947de873
dboren <danielboren@att.net>
2021-10-22 15:56:29 -0500

    added logic using split, reverser, and join methods to compare input to reversed input. Not testing successfully yet

--- W6/01-is-palindrome/Unsolved/is-palindrome.js
+++ W6/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,25 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+
+    let input = str;
+
+    function invertStr(input) {
+
+        let strArr = split(input);
+
+        let invArr = reverse(strArr);
+
+        let inverseStr = join(invArr);
+
+        return inverseStr
+        
+    }
+
+    invertStr(input);
+
+    if (input === inverseStr) {
+        
+        return true
+    }
+};
