6e9f32e4b2a999b131a03b55fe929349b4e8f601
dboren <danielboren@att.net>
2021-10-30 16:51:42 -0500

    added decrementing for loop to generate facrotial for num. Testing successfully

--- W6/02-factorial/Unsolved/factorial.js
+++ W6/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,12 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+
+    var result = 1;
+
+    for (i = num; i > 0; i--) {
+        result = i * result;
+    }
+
+    return result;
+};
