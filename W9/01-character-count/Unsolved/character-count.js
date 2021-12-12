961d7be8accf615c7c7374086f925c1cf13321c9
dboren <danielboren@att.net>
2021-12-12 14:40:32 -0600

    initialized empty result object and added for loop to log each character of input string

--- W9/01-character-count/Unsolved/character-count.js
+++ W9/01-character-count/Unsolved/character-count.js
@@ -1,12 +1,12 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-// var characterCount = function(str) {
+var characterCount = function(str) {
 
-//     var result = {};
+    var result = {};
 
-//     for (i = 0; i < str.length; i++) {
+    for (i = 0; i < str.length; i++) {
 
-//         var currentChar = str[i];
-//         console.log(currentChar);
-//     }
-// };
+        var currentChar = str[i];
+        console.log(currentChar);
+    }
+};
