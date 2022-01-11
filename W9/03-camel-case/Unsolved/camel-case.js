d4f418fdb0e64e8440401ea9c1b1db0f90f6cf3f
dboren <danielboren@att.net>
2022-01-10 21:55:47 -0600

    added logic to if i is a space block to make i + 1 uppercase, remove i, and console log str, but the if block is never being hit according to the console

--- W9/03-camel-case/Unsolved/camel-case.js
+++ W9/03-camel-case/Unsolved/camel-case.js
@@ -1,13 +1,26 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
 var camelCase = function(str) {
+
+    var camelString = []
+
     for (var i = 0; i < str.length; i++) {
 
         if (str[i] == " " ) {
 
             // upperify str[i+1] and delete str[i]
 
+            str[i+1].toUpperCase();
+
+            str[i].pop()
+
+            console.log("string so far: " + str)
+
         }
+
+        console.log("new string: " + str)
+
+        return
 
     }
 };
