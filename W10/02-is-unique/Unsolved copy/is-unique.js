3569f8dce0b8ea97acc8ede2943149f77fabf4bb
Daniel Boren <danielboren52@gmail.com>
2022-07-04 16:32:26 -0500

    added starting for loop iterator for isUnique test

--- W10/02-is-unique/Unsolved copy/is-unique.js
+++ W10/02-is-unique/Unsolved copy/is-unique.js
@@ -1,11 +1,4 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {
-
-    let counter = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        
-    }
-};
+var isUnique = function(arr) {};
