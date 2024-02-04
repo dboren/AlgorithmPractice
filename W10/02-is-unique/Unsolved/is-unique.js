ae0ac7e9edce060e1998569f647126630045b3f4
Daniel Boren <danielboren52@gmail.com>
2024-02-04 17:47:39 -0600

    added console logs to solution to examine how the loop is performing

--- W10/02-is-unique/Unsolved/is-unique.js
+++ W10/02-is-unique/Unsolved/is-unique.js
@@ -2,25 +2,58 @@
 // Return `true` is no number appears in the array more than once, else return `false`
 
 var isUnique = function(arr) {
+    var numMap = {};
+    console.info(numMap)
+  
+    for (var i = 0; i < arr.length; i++) {
+      var currentNumber = arr[i];
 
-    let counter = 0;
-    let selector = 1;
-
-
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] == arr[i + selector]) {
-
-            counter++;
-            selector++;
-
-        } selector++;
-
-    }
-
-    if (counter > 0) {
-
+      console.info("map: " + numMap);
+      console.log("current: " + currentNumber);
+  
+      if (numMap[currentNumber] === true) {
         return false;
+      }
+  
+      numMap[currentNumber] = true;
+    }
+  
+    return true;
+  };
 
-    } else return true;
-};
+
+// var isUnique = function(arr) {
+
+//     let counter = 0;
+//     let selector = 1;
+
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         console.log("arr[i]: " + arr[i]);
+
+//         console.log(arr[i] == arr[i + selector]);
+
+//         if (arr[i] == arr[i + selector]) {
+
+
+
+//             counter++;
+//             selector++;
+
+
+//             console.log("counter: " + counter);
+//             console.log("sleector: " + selector);
+
+//         } selector++;
+
+
+
+//     }
+
+//     if (counter > 0) {
+
+//         return false;
+
+//     } else return true;
+// };
